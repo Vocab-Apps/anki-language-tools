80c9bf7b7057efae6188bbf34d625f2a478dd26d
luc wastiaux <luc.wastiaux@airpost.net>
2020-12-17 18:04:38 +0800

    cleanups

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ class LanguageTools():
         query = f'did:{deck.id} mid:{note_type.id}'
         notes = mw.col.find_notes(query)
         if len(notes) > 0:  
-            print(f'found {len(notes)} notes in {deck.name}, {note_type.name}')
+            # print(f'found {len(notes)} notes in {deck.name}, {note_type.name}')
             if len(notes) < sample_size:
                 random_note_ids = notes
             else:
@@ -54,7 +54,7 @@ class LanguageTools():
             fields = model['flds']
             for field in fields:
                 field_name = field['name']
-                print(f'  field: {field_name}, performing detection')
+                # print(f'  field: {field_name}, performing detection')
                 field_sample = [mw.col.getNote(x)[field_name] for x in random_note_ids]
                 response = requests.post(self.base_url + '/detect', json={
                         'text_list': field_sample
@@ -89,7 +89,7 @@ class LanguageTools():
         model_name = model['name']
         deck = mw.col.decks.get(deck_id)
         deck_name = deck['name']
-        print(f'model_name {model_name} deck_name {deck_name} field_name {field_name}')
+        # print(f'model_name {model_name} deck_name {deck_name} field_name {field_name}')
         return self.config[LanguageTools.CONFIG_DECK_LANGUAGES][model_name][deck_name][field_name]
 
     def get_wanted_languages(self):
@@ -109,117 +109,10 @@ languagetools = LanguageTools()
 languagetools.initialize()
 
 
-def retrieveLanguages():
-    response = requests.get(base_url + '/language_list')
-    result = json.loads(response.content)
-    print(result)
-
-def detectLanguage():
-    print('detectLanguage')
-    query = "mid:1354424015760 deck:Mandarin"
-    note_ids = mw.col.find_notes(query)
-
-
-    random_note_ids = random.sample(note_ids, 10)
-
-    english_fields = [mw.col.getNote(x)['English'] for x in random_note_ids]
-    chinese_fields = [mw.col.getNote(x)['Chinese'] for x in random_note_ids]
-    # print(english_fields)
-
-    response = requests.post(base_url + '/detect', json={
-            'text_list': chinese_fields
-    })
-
-    data = json.loads(response.content)
-    print(data)
-
-
-
-def listCollection():
-
-    # get all of the notetypes
-    note_types = mw.col.models.all_names_and_ids()
-
-    for note_type in note_types:
-        query = f"mid:{note_type.id}"
-        print(query)
-        notes = mw.col.find_notes(query)
-        print(f'found {len(notes)} notes with model {note_type.name}')
-    print(note_types)
-
-    deck_list = mw.col.decks.all_names_and_ids()
-    first_deck = deck_list[0]
-    first_deck_id = first_deck.id
-    # print(deck_list)
-
-    # obtain the deck
-    deck = mw.col.decks.get(first_deck_id)
-
-    print(type(deck))
-    print(deck.keys())
-    print(deck.values())
-
-def testFunction():
-    # print(f'testFunction')
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
-
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-action.triggered.connect(testFunction)
-# and add it to the tools menu
-mw.form.menuTools.addAction(action)
-
-
-action = QAction("retrieveLanguages", mw)
-action.triggered.connect(retrieveLanguages)
-mw.form.menuTools.addAction(action)
-
-action = QAction("listCollection", mw)
-action.triggered.connect(listCollection)
-mw.form.menuTools.addAction(action)
-
-action = QAction("detectLanguage", mw)
-action.triggered.connect(languagetools.perform_language_detection)
-mw.form.menuTools.addAction(action)
-
-
-def translate_text_azure(source_text):
-    url = base_url + '/translate'
-    query_json = {
-        'text': source_text,
-        'service': 'Azure',
-        'from_language_key': 'zh-Hans',
-        'to_language_key': 'en'
-    }
-    print(query_json)
-    response = requests.post(url, json=query_json)
-    data = json.loads(response.content)
-    print(data)
-    print(f"translation: {data['translated_text']}")
-
-def translate_text_google(source_text):
-    url = base_url + '/translate'
-    query_json = {
-        'text': source_text,
-        'service': 'Google',
-        'from_language_key': 'zh-CN',
-        'to_language_key': 'en'
-    }
-    print(query_json)
-    response = requests.post(url, json=query_json)
-    data = json.loads(response.content)
-    print(data)
-    print(f"translation: {data['translated_text']}")    
-
 # add context menu handler
 
 def show_translation(source_text, from_language, to_language):
-    print(f'translate {source_text} from {from_language} to {to_language}')
+    # print(f'translate {source_text} from {from_language} to {to_language}')
     result = languagetools.get_translation(source_text, from_language, to_language)
 
     translations = ''
@@ -235,7 +128,6 @@ def on_context_menu(web_view, menu):
     selected_text = web_view.selectedText()
     current_field_num = web_view.editor.currentField
     note = web_view.editor.note
-    print(note)
     model_id = note.mid
     model = mw.col.models.get(model_id)
     field_name = model['flds'][current_field_num]['name']
@@ -244,8 +136,6 @@ def on_context_menu(web_view, menu):
         deck_id = card.did
 
         language = languagetools.get_language(deck_id, model_id, field_name)
-
-        print(f'language for {field_name}: {language}')
 
         source_text_max_length = 25
         source_text = selected_text
