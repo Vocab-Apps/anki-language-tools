89126b28cdf46427378a3bba2dd3c37a9689142e
luc wastiaux <luc.wastiaux@airpost.net>
2021-04-04 15:18:56 +0800

    add mock interfaces for testing

---
+++ testing_utils.py
@@ -0,0 +1,49 @@
+import constants
+
+class MockAnkiUtils():
+    def __init__(self, config):
+        self.config = config
+
+    def get_config(self):
+        return self.config
+
+    def get_green_stylesheet(self):
+        return constants.GREEN_STYLESHEET
+
+    def get_red_stylesheet(self):
+        return constants.RED_STYLESHEET
+
+    def play_anki_sound_tag(self, text):
+        self.last_played_sound_tag = text
+
+class MockCloudLanguageTools():
+    def __init__(self):
+        self.language_list = {
+            'en': 'English',
+            'zh_cn': 'Chinese'
+        }
+        self.translation_language_list = [
+            {
+                'service': "Azure",
+                'language_code': "en",
+                'language_name': "English",
+                'language_id': "en"
+            },
+            {
+                'service': "Azure",
+                'language_code': "zh_cn",
+                'language_name': "Chinese",
+                'language_id': "zh-hans"
+            },            
+        ]
+        self.transliteration_language_list = [] # todo fill this out
+
+
+    def get_language_list(self):
+        return self.language_list
+
+    def get_translation_language_list(self):
+        return self.translation_language_list
+
+    def get_transliteration_language_list(self):
+        return self.transliteration_language_list
