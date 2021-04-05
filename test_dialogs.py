aacf8f8f96d15d96612ef4843423bb006f3f2dcd
luc wastiaux <luc.wastiaux@airpost.net>
2021-04-05 15:53:16 +0800

    move test config generator to testing_utils

--- test_dialogs.py
+++ test_dialogs.py
@@ -19,101 +19,19 @@ def assert_combobox_items_equal(combobox, expected_items):
     expected_items.sort()
     assert combobox_items == expected_items
 
-class TestConfigGenerator():
-    def __init__(self):
-        self.deck_id = 42001
-        self.model_id = 43001
-        self.model_name = 'note-type'
-        self.deck_name = 'deck 1'
-        self.field_chinese = 'Chinese'
-        self.field_english = 'English'
-        self.field_sound = 'Sound'
-        self.all_fields = [self.field_chinese, self.field_english, self.field_sound]
 
-        self.chinese_voice_key = 'chinese voice'
-        self.chinese_voice_description = 'this is a chinese voice'
-
-    def get_default_config(self):
-        languagetools_config = {
-            'api_key': 'yoyo',
-            constants.CONFIG_DECK_LANGUAGES: {
-                self.model_name: {
-                    self.deck_name: {
-                        self.field_chinese: 'zh_cn',
-                        self.field_english: 'en',
-                        self.field_sound: 'sound'
-                    }
-                }
-            },
-            constants.CONFIG_VOICE_SELECTION: {
-                'zh_cn': {
-                    'voice_key': self.chinese_voice_key,
-                    'voice_description': self.chinese_voice_description
-                }
-            }
-
-        }
-        return languagetools_config
-
-    def get_config_no_language_mapping(self):
-        base_config = self.get_default_config()
-        base_config[constants.CONFIG_DECK_LANGUAGES] = {}
-        return base_config
-
-    def get_config_batch_audio(self):
-        base_config = self.get_default_config()
-        base_config[constants.CONFIG_BATCH_AUDIO] = {
-            self.model_name: {
-                self.deck_name: {
-                   self.field_sound: self.field_chinese
-                }
-            }
-        }
-        return base_config
-
-    def get_model_map(self):
-        return {
-            self.model_id: {
-                'name': self.model_name,
-                'flds': [
-                    {'name': self.field_chinese},
-                    {'name': self.field_english},
-                    {'name': self.field_sound}
-                ]
-            }
-        }
-    
-    def get_deck_map(self):
-        return {
-            self.deck_id: {
-                'name': self.deck_name
-            }
-        }
-
-    def get_deckid_modelid_pairs(self):
-        return [
-            [self.deck_id, self.model_id]
-        ]
 
 
 def test_add_audio(qtbot):
     # pytest test_dialogs.py -rPP -k test_add_audio
 
-    config_gen = TestConfigGenerator()
-    languagetools_config = config_gen.get_default_config()
-    mock_cloudlanguagetools = testing_utils.MockCloudLanguageTools()
+    config_gen = testing_utils.TestConfigGenerator()
 
     # test 1 - everything setup but no prior setting
     # ----------------------------------------------
+    mock_language_tools = config_gen.build_languagetools_instance('default')
 
-    anki_utils = testing_utils.MockAnkiUtils(languagetools_config)
-    deckutils = deck_utils.DeckUtils(anki_utils)
-    mock_language_tools = languagetools.LanguageTools(anki_utils, deckutils, mock_cloudlanguagetools)
-    mock_language_tools.initialize()
-
-    anki_utils.models = config_gen.get_model_map()
-    anki_utils.decks = config_gen.get_deck_map()
-    deck_note_type = deckutils.build_deck_note_type(config_gen.deck_id, config_gen.model_id)
+    deck_note_type = mock_language_tools.deck_utils.build_deck_note_type(config_gen.deck_id, config_gen.model_id)
 
     note_id_list = [42, 43]
     add_audio_dialog = dialogs.AddAudioDialog(mock_language_tools, deck_note_type, note_id_list)
@@ -126,13 +44,7 @@ def test_add_audio(qtbot):
 
     # test 2 - some defaults already exist
     # ------------------------------------
-    languagetools_config = config_gen.get_config_batch_audio()
-    anki_utils = testing_utils.MockAnkiUtils(languagetools_config)
-    anki_utils.models = config_gen.get_model_map()
-    anki_utils.decks = config_gen.get_deck_map()    
-    deckutils = deck_utils.DeckUtils(anki_utils)
-    mock_language_tools = languagetools.LanguageTools(anki_utils, deckutils, mock_cloudlanguagetools)    
-    mock_language_tools.initialize()
+    mock_language_tools = config_gen.build_languagetools_instance('batch_audio')
 
     add_audio_dialog = dialogs.AddAudioDialog(mock_language_tools, deck_note_type, note_id_list)
     add_audio_dialog.setupUi()
@@ -145,13 +57,7 @@ def test_add_audio(qtbot):
 
     # test 3 - no language mapping done for any field
     # -----------------------------------------------
-    languagetools_config = config_gen.get_config_no_language_mapping()
-    anki_utils = testing_utils.MockAnkiUtils(languagetools_config)
-    anki_utils.models = config_gen.get_model_map()
-    anki_utils.decks = config_gen.get_deck_map()    
-    deckutils = deck_utils.DeckUtils(anki_utils)
-    mock_language_tools = languagetools.LanguageTools(anki_utils, deckutils, mock_cloudlanguagetools)
-    mock_language_tools.initialize()
+    mock_language_tools = config_gen.build_languagetools_instance('no_language_mapping')
 
     # add_audio_dialog = dialogs.AddAudioDialog(mock_language_tools, deck_note_type, note_id_list)
     testcase_instance = unittest.TestCase()
--- testing_utils.py
+++ testing_utils.py
@@ -1,4 +1,6 @@
 import constants
+import deck_utils
+import languagetools
 
 class MockAnkiUtils():
     def __init__(self, config):
@@ -62,3 +64,108 @@ class MockCloudLanguageTools():
         return {
             'key_valid': True
         }     
+
+class TestConfigGenerator():
+    def __init__(self):
+        self.deck_id = 42001
+        self.model_id = 43001
+        self.model_name = 'note-type'
+        self.deck_name = 'deck 1'
+        self.field_chinese = 'Chinese'
+        self.field_english = 'English'
+        self.field_sound = 'Sound'
+        self.all_fields = [self.field_chinese, self.field_english, self.field_sound]
+
+        self.chinese_voice_key = 'chinese voice'
+        self.chinese_voice_description = 'this is a chinese voice'
+
+    # different languagetools configs available
+    # =========================================
+
+    def get_default_config(self):
+        languagetools_config = {
+            'api_key': 'yoyo',
+            constants.CONFIG_DECK_LANGUAGES: {
+                self.model_name: {
+                    self.deck_name: {
+                        self.field_chinese: 'zh_cn',
+                        self.field_english: 'en',
+                        self.field_sound: 'sound'
+                    }
+                }
+            },
+            constants.CONFIG_VOICE_SELECTION: {
+                'zh_cn': {
+                    'voice_key': self.chinese_voice_key,
+                    'voice_description': self.chinese_voice_description
+                }
+            }
+
+        }
+        return languagetools_config
+
+    def get_config_no_language_mapping(self):
+        base_config = self.get_default_config()
+        base_config[constants.CONFIG_DECK_LANGUAGES] = {}
+        return base_config
+
+    def get_config_batch_audio(self):
+        base_config = self.get_default_config()
+        base_config[constants.CONFIG_BATCH_AUDIO] = {
+            self.model_name: {
+                self.deck_name: {
+                   self.field_sound: self.field_chinese
+                }
+            }
+        }
+        return base_config
+
+    def get_languagetools_config(self, scenario):
+
+        fn_map = {
+            'default': self.get_default_config,
+            'no_language_mapping': self.get_config_no_language_mapping,
+            'batch_audio': self.get_config_batch_audio
+        }
+
+        fn_instance = fn_map[scenario]
+        return fn_instance()
+
+
+    def get_model_map(self):
+        return {
+            self.model_id: {
+                'name': self.model_name,
+                'flds': [
+                    {'name': self.field_chinese},
+                    {'name': self.field_english},
+                    {'name': self.field_sound}
+                ]
+            }
+        }
+    
+    def get_deck_map(self):
+        return {
+            self.deck_id: {
+                'name': self.deck_name
+            }
+        }
+
+    def get_deckid_modelid_pairs(self):
+        return [
+            [self.deck_id, self.model_id]
+        ]        
+
+    def build_languagetools_instance(self, scenario):
+        languagetools_config = self.get_languagetools_config(scenario)
+        mock_cloudlanguagetools = MockCloudLanguageTools()
+
+        anki_utils = MockAnkiUtils(languagetools_config)
+        deckutils = deck_utils.DeckUtils(anki_utils)
+        mock_language_tools = languagetools.LanguageTools(anki_utils, deckutils, mock_cloudlanguagetools)
+        mock_language_tools.initialize()
+
+        anki_utils.models = self.get_model_map()
+        anki_utils.decks = self.get_deck_map()
+
+        return mock_language_tools
