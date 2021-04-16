314315caf57caa42608dc5dcb0af99010c29d40d
luc wastiaux <luc.wastiaux@airpost.net>
2021-04-16 13:49:31 +0800

    add exception handler

--- editor_processing.py
+++ editor_processing.py
@@ -9,48 +9,51 @@ else:
     from . import dialog_choosetranslation
 
 def process_choosetranslation(editor, languagetools, str):
-    logging.debug(f'choosetranslation command: [{str}]')
-    components = str.split(':')
-    field_index_str = components[1]
-    field_index = int(field_index_str)
+    try:
+        logging.debug(f'choosetranslation command: [{str}]')
+        components = str.split(':')
+        field_index_str = components[1]
+        field_index = int(field_index_str)
 
-    note = editor.note
-    current_translation_text = note.fields[field_index]
+        note = editor.note
+        current_translation_text = note.fields[field_index]
 
-    deck_note_type = languagetools.deck_utils.build_deck_note_type_from_editor(editor)
+        deck_note_type = languagetools.deck_utils.build_deck_note_type_from_editor(editor)
 
-    target_dntf = languagetools.deck_utils.get_dntf_from_fieldindex(deck_note_type, field_index)
-    translation_from_field = languagetools.get_batch_translation_setting_field(target_dntf)
-    from_field = translation_from_field['from_field']
-    from_dntf = languagetools.deck_utils.build_dntf_from_dnt(deck_note_type, from_field)
-    from_text = note[from_field]
+        target_dntf = languagetools.deck_utils.get_dntf_from_fieldindex(deck_note_type, field_index)
+        translation_from_field = languagetools.get_batch_translation_setting_field(target_dntf)
+        from_field = translation_from_field['from_field']
+        from_dntf = languagetools.deck_utils.build_dntf_from_dnt(deck_note_type, from_field)
+        from_text = note[from_field]
 
-    logging.debug(f'from field: {from_dntf} target field: {target_dntf}')
+        logging.debug(f'from field: {from_dntf} target field: {target_dntf}')
 
-    # get to and from languages
-    from_language = languagetools.get_language(from_dntf)
-    to_language = languagetools.get_language(target_dntf)
-    if from_language == None:
-        raise errors.FieldLanguageMappingError(from_dntf)
-    if to_language == None:
-        raise errors.FieldLanguageMappingError(target_dntf)
+        # get to and from languages
+        from_language = languagetools.get_language(from_dntf)
+        to_language = languagetools.get_language(target_dntf)
+        if from_language == None:
+            raise errors.FieldLanguageMappingError(from_dntf)
+        if to_language == None:
+            raise errors.FieldLanguageMappingError(target_dntf)
 
-    def load_translation_all():
-        return languagetools.get_translation_all(from_text, from_language, to_language)
+        def load_translation_all():
+            return languagetools.get_translation_all(from_text, from_language, to_language)
 
-    def get_done_callback(from_text, from_language, to_language, editor, field_index):
-        def load_translation_all_done(fut):
-            languagetools.anki_utils.stop_progress_bar()
-            data = fut.result()
-            # logging.debug(f'all translations: {data}')
-            dialog = dialog_choosetranslation.prepare_dialog(languagetools, from_text, from_language, to_language, data)
-            retval = languagetools.anki_utils.display_dialog(dialog)
-            if retval == True:
-                chosen_translation = dialog.selected_translation
-                #logging.debug(f'chosen translation: {chosen_translation}')
-                languagetools.anki_utils.editor_set_field_value(editor, field_index, chosen_translation)
+        def get_done_callback(from_text, from_language, to_language, editor, field_index):
+            def load_translation_all_done(fut):
+                languagetools.anki_utils.stop_progress_bar()
+                data = fut.result()
+                # logging.debug(f'all translations: {data}')
+                dialog = dialog_choosetranslation.prepare_dialog(languagetools, from_text, from_language, to_language, data)
+                retval = languagetools.anki_utils.display_dialog(dialog)
+                if retval == True:
+                    chosen_translation = dialog.selected_translation
+                    #logging.debug(f'chosen translation: {chosen_translation}')
+                    languagetools.anki_utils.editor_set_field_value(editor, field_index, chosen_translation)
 
-        return load_translation_all_done
+            return load_translation_all_done
 
-    languagetools.anki_utils.show_progress_bar("retrieving all translations")
-    languagetools.anki_utils.run_in_background(load_translation_all, get_done_callback(from_text, from_language, to_language, editor, field_index))
+        languagetools.anki_utils.show_progress_bar("retrieving all translations")
+        languagetools.anki_utils.run_in_background(load_translation_all, get_done_callback(from_text, from_language, to_language, editor, field_index))
+    except Exception as e:
+        languagetools.anki_utils.critical_message(str(e))
