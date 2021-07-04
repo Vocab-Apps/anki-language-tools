7d003b15d0e48424ee848831a8237286130b18fb
luc wastiaux <luc.wastiaux@airpost.net>
2021-07-04 17:08:04 +0800

    rename label

--- dialog_textprocessing.py
+++ dialog_textprocessing.py
@@ -227,7 +227,7 @@ class TextProcessingDialog(PyQt5.QtWidgets.QDialog):
         hlayout.addWidget(self.add_replace_simple_button)
         self.add_replace_regex_button = PyQt5.QtWidgets.QPushButton('Add Regex Text Replacement Rule')
         hlayout.addWidget(self.add_replace_regex_button)
-        self.remove_replace_button = PyQt5.QtWidgets.QPushButton('Remove Selected Text Replacement Rule')
+        self.remove_replace_button = PyQt5.QtWidgets.QPushButton('Remove Selected Rule')
         hlayout.addWidget(self.remove_replace_button)
         vlayout.addLayout(hlayout)
 
