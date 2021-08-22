0d9a4652bfcbfef61be88ea2b16a2bb2da4e18fa
luc wastiaux <luc.wastiaux@airpost.net>
2021-08-22 08:11:06 +0800

    include sentry sdk

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import sys
+import os
 if hasattr(sys, '_pytest_mode'):
     # called from within a test run
     pass
@@ -9,6 +10,17 @@ else:
     from . import anki_utils
     from . import deck_utils
     from . import cloudlanguagetools
+    
+    # setup sentry crash reporting
+    addon_dir = os.path.dirname(os.path.realpath(__file__))
+    external_dir = os.path.join(addon_dir, 'external')
+    sys.path.append(external_dir)
+    import sentry_sdk
+
+    sentry_sdk.init(
+        "https://dbee54f0eff84f0db037e995ae46df11@o968582.ingest.sentry.io/5920286",
+        traces_sample_rate=1.0
+    )
 
     ankiutils = anki_utils.AnkiUtils()
     deckutils = deck_utils.DeckUtils(ankiutils)
