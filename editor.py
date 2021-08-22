2fb7f0b00b3f431e507c6ae4344ffca6eee1fff4
luc wastiaux <luc.wastiaux@airpost.net>
2021-08-22 08:20:45 +0800

    catch exceptions in various places

--- editor.py
+++ editor.py
@@ -3,6 +3,7 @@ import json
 import typing
 import urllib.parse
 import logging
+import sentry_sdk
 from typing import List, Dict
 
 # anki imports
@@ -59,115 +60,126 @@ def init(languagetools):
 
 
     def loadNote(editor: aqt.editor.Editor):
-        note = editor.note
-        deck_note_type = languagetools.deck_utils.build_deck_note_type_from_editor(editor)
+        try:
+            note = editor.note
+            deck_note_type = languagetools.deck_utils.build_deck_note_type_from_editor(editor)
 
-        model = note.model()
-        fields = model['flds']
-        field_options = []
-        for index, field in enumerate(fields):
-            field_name = field['name']
+            model = note.model()
+            fields = model['flds']
+            field_options = []
+            for index, field in enumerate(fields):
+                field_name = field['name']
 
-            field_type ='regular'
-            # is this field a sound field ?
-            dntf = languagetools.deck_utils.build_dntf_from_dnt(deck_note_type, field_name)
-            field_language = languagetools.get_language(dntf)
-            if field_language != None:
-                if languagetools.get_batch_translation_setting_field(dntf) != None:
-                    # add translation settings
-                    field_type = 'translation'
-                elif field_language == constants.SpecialLanguage.sound.name:
-                    # add_play_sound_collection(editor, index, field_name)
-                    field_type = 'sound'
-                elif field_language in languagetools.get_voice_selection_settings(): # is there a voice associated with this language ?
-                    # add_tts_speak(editor, index, field_name)
-                    field_type = 'language'
-            field_options.append(field_type)
-        live_updates = languagetools.get_apply_updates_automatically()
-        typing_delay = languagetools.get_live_update_delay()
-        configure_editor_fields(editor, field_options, live_updates, typing_delay)
+                field_type ='regular'
+                # is this field a sound field ?
+                dntf = languagetools.deck_utils.build_dntf_from_dnt(deck_note_type, field_name)
+                field_language = languagetools.get_language(dntf)
+                if field_language != None:
+                    if languagetools.get_batch_translation_setting_field(dntf) != None:
+                        # add translation settings
+                        field_type = 'translation'
+                    elif field_language == constants.SpecialLanguage.sound.name:
+                        # add_play_sound_collection(editor, index, field_name)
+                        field_type = 'sound'
+                    elif field_language in languagetools.get_voice_selection_settings(): # is there a voice associated with this language ?
+                        # add_tts_speak(editor, index, field_name)
+                        field_type = 'language'
+                field_options.append(field_type)
+            live_updates = languagetools.get_apply_updates_automatically()
+            typing_delay = languagetools.get_live_update_delay()
+            configure_editor_fields(editor, field_options, live_updates, typing_delay)
+
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            raise
         
 
     def onBridge(handled, str, editor):
-        # logging.debug(f'bridge str: {str}')
+        try:
+            # logging.debug(f'bridge str: {str}')
 
-        # return handled # don't do anything for now
-        if not isinstance(editor, aqt.editor.Editor):
-            return handled
+            # return handled # don't do anything for now
+            if not isinstance(editor, aqt.editor.Editor):
+                return handled
 
-        if str.startswith('playsoundcollection:'):
-            logging.debug(f'playsoundcollection command: [{str}]')
-            components = str.split(':')
-            field_index_str = components[1]
-            field_index = int(field_index_str)
+            if str.startswith('playsoundcollection:'):
+                logging.debug(f'playsoundcollection command: [{str}]')
+                components = str.split(':')
+                field_index_str = components[1]
+                field_index = int(field_index_str)
 
-            note = editor.note
-            sound_tag = note.fields[field_index]
-            logging.debug(f'sound tag: {sound_tag}')
+                note = editor.note
+                sound_tag = note.fields[field_index]
+                logging.debug(f'sound tag: {sound_tag}')
 
-            languagetools.anki_utils.play_anki_sound_tag(sound_tag)
-            return handled
+                languagetools.anki_utils.play_anki_sound_tag(sound_tag)
+                return handled
 
-        if str.startswith('choosetranslation:'):
-            editor_manager.process_choosetranslation(editor, str)
-            return True, None
+            if str.startswith('choosetranslation:'):
+                editor_manager.process_choosetranslation(editor, str)
+                return True, None
 
-        if str.startswith('ttsspeak:'):
-            logging.debug(f'ttsspeak command: [{str}]')
-            components = str.split(':')
-            field_index_str = components[1]
-            field_index = int(field_index_str)
+            if str.startswith('ttsspeak:'):
+                logging.debug(f'ttsspeak command: [{str}]')
+                components = str.split(':')
+                field_index_str = components[1]
+                field_index = int(field_index_str)
 
-            note = editor.note
-            source_text = note.fields[field_index]
-            logging.debug(f'source_text: {source_text}')
+                note = editor.note
+                source_text = note.fields[field_index]
+                logging.debug(f'source_text: {source_text}')
 
-            try:
+                try:
 
-                from_deck_note_type_field = languagetools.deck_utils.editor_get_dntf(editor, field_index)
+                    from_deck_note_type_field = languagetools.deck_utils.editor_get_dntf(editor, field_index)
 
-                # do we have a voice set ?
-                field_language = languagetools.get_language(from_deck_note_type_field)
-                if field_language == None:
-                    raise errors.AnkiNoteEditorError(f'No language set for field {from_deck_note_type_field}')
-                voice_selection_settings = languagetools.get_voice_selection_settings()
-                if field_language not in voice_selection_settings:
-                    raise errors.AnkiNoteEditorError(f'No voice set for language {languagetools.get_language_name(field_language)}')
-                voice = voice_selection_settings[field_language]
+                    # do we have a voice set ?
+                    field_language = languagetools.get_language(from_deck_note_type_field)
+                    if field_language == None:
+                        raise errors.AnkiNoteEditorError(f'No language set for field {from_deck_note_type_field}')
+                    voice_selection_settings = languagetools.get_voice_selection_settings()
+                    if field_language not in voice_selection_settings:
+                        raise errors.AnkiNoteEditorError(f'No voice set for language {languagetools.get_language_name(field_language)}')
+                    voice = voice_selection_settings[field_language]
 
-                def play_audio(languagetools, source_text, voice):
-                    voice_key = voice['voice_key']
-                    service = voice['service']
-                    language_code = voice['language_code']
+                    def play_audio(languagetools, source_text, voice):
+                        voice_key = voice['voice_key']
+                        service = voice['service']
+                        language_code = voice['language_code']
 
-                    try:
-                        filename = languagetools.get_tts_audio(source_text, service, language_code, voice_key, {})
-                        if filename != None:
-                            aqt.sound.av_player.play_file(filename)
-                    except errors.LanguageToolsRequestError as err:
+                        try:
+                            filename = languagetools.get_tts_audio(source_text, service, language_code, voice_key, {})
+                            if filename != None:
+                                aqt.sound.av_player.play_file(filename)
+                        except errors.LanguageToolsRequestError as err:
+                            pass
+
+                    def play_audio_done(future_result):
                         pass
 
-                def play_audio_done(future_result):
-                    pass
+                    aqt.mw.taskman.run_in_background(lambda: play_audio(languagetools, source_text, voice), lambda x: play_audio_done(x))
 
-                aqt.mw.taskman.run_in_background(lambda: play_audio(languagetools, source_text, voice), lambda x: play_audio_done(x))
+                except errors.AnkiNoteEditorError as e:
+                    # logging.error('Could not speak', exc_info=True)
+                    aqt.utils.showCritical(repr(e))
 
-            except errors.AnkiNoteEditorError as e:
-                # logging.error('Could not speak', exc_info=True)
-                aqt.utils.showCritical(repr(e))
+                return handled
+
+            if str.startswith("languagetools:"):
+                editor_manager.process_command(editor, str)
+                return True, None
+
+            if str.startswith("key:"):
+                # user updated field, see if we need to do any transformations
+                editor_manager.process_field_update(editor, str)
+
 
             return handled
 
-        if str.startswith("languagetools:"):
-            editor_manager.process_command(editor, str)
-            return True, None
-
-        if str.startswith("key:"):
-            # user updated field, see if we need to do any transformations
-            editor_manager.process_field_update(editor, str)
-
-
-        return handled
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            raise        
+        
 
 
     aqt.gui_hooks.webview_will_set_content.append(on_webview_will_set_content)
