e59b7623bde239b12d5e8f33794dcfbaf081c216
luc-languagetools <languagetools@mailc.net>
2022-04-09 12:00:55 +0800

    qt6 compatibility

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 import sys
 import logging
+import anki.lang
 
 def pytest_configure(config):
     sys._pytest_mode = True
     logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', 
                         datefmt='%Y%m%d-%H:%M:%S',
                         level=logging.DEBUG)    
+    # required to access some anki functions such as anki.utils.html_to_text_line
+    anki.lang.set_lang('en_US')                        
 
 def pytest_unconfigure(config):
     del sys._pytest_mode
