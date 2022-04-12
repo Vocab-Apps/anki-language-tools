478b393b88f1de7dbd6a4324d3f3eab3b410d032
luc-languagetools <languagetools@mailc.net>
2022-04-12 21:07:44 +0800

    working tests

--- test_editor.py
+++ test_editor.py
@@ -10,7 +10,6 @@ import editor_processing
 import constants
 import errors
 
-@pytest.mark.skip(reason="will be reimplemented")
 def test_process_choosetranslation(qtbot):
     # pytest test_editor.py -rPP -k test_process_choosetranslation
 
@@ -24,17 +23,15 @@ def test_process_choosetranslation(qtbot):
         }
     }
 
-    bridge_str = 'choosetranslation:1'
-
     # when the choose translation dialog comes up, we should pick serviceB
     mock_language_tools.anki_utils.display_dialog_behavior = 'choose_serviceB'
 
     editor = config_gen.get_mock_editor_with_note(config_gen.note_id_1)
     editor_manager = editor_processing.EditorManager(mock_language_tools)
-    editor_manager.process_choosetranslation(editor,  bridge_str)
+    editor_manager.run_choose_translation(editor, 'English')
 
     assert len(mock_language_tools.anki_utils.editor_set_field_value_calls) == 1
-    assert mock_language_tools.anki_utils.editor_set_field_value_calls[0]['field_index'] == 1
+    assert mock_language_tools.anki_utils.editor_set_field_value_calls[0]['field_name'] == 'English'
     assert mock_language_tools.anki_utils.editor_set_field_value_calls[0]['text'] == 'second translation B'
 
 def test_process_choosetranslation_empty(qtbot):
@@ -43,11 +40,10 @@ def test_process_choosetranslation_empty(qtbot):
     config_gen = testing_utils.TestConfigGenerator()
     mock_language_tools = config_gen.build_languagetools_instance('batch_translation')
 
-    bridge_str = 'choosetranslation:1'
 
     editor = config_gen.get_mock_editor_with_note(config_gen.note_id_3)
     editor_manager = editor_processing.EditorManager(mock_language_tools)
-    editor_manager.process_choosetranslation(editor,  bridge_str)
+    editor_manager.run_choose_translation(editor, 'English')
 
     assert len(mock_language_tools.anki_utils.editor_set_field_value_calls) == 0
 
@@ -61,15 +57,13 @@ def test_process_choosetranslation_request_error(qtbot):
     config_gen = testing_utils.TestConfigGenerator()
     mock_language_tools = config_gen.build_languagetools_instance('batch_translation')
 
-    bridge_str = 'choosetranslation:1'
-
     mock_language_tools.cloud_language_tools.translation_unhandled_exception_map = {
         '老人家': 'unhandled exception translate all'
     }
 
     editor = config_gen.get_mock_editor_with_note(config_gen.note_id_1)
     editor_manager = editor_processing.EditorManager(mock_language_tools)
-    editor_manager.process_choosetranslation(editor,  bridge_str)
+    editor_manager.run_choose_translation(editor, 'English')
 
     assert len(mock_language_tools.anki_utils.editor_set_field_value_calls) == 0
 
@@ -90,14 +84,12 @@ def test_process_choosetranslation_cancel(qtbot):
         }
     }
 
-    bridge_str = 'choosetranslation:1'
-
     # when the choose translation dialog comes up, we should pick serviceB
     mock_language_tools.anki_utils.display_dialog_behavior = 'cancel'
 
     editor = config_gen.get_mock_editor_with_note(config_gen.note_id_1)
     editor_manager = editor_processing.EditorManager(mock_language_tools)
-    editor_manager.process_choosetranslation(editor,  bridge_str)    
+    editor_manager.run_choose_translation(editor, 'English')
 
     assert len(mock_language_tools.anki_utils.editor_set_field_value_calls) == 0
     
